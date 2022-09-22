5373a7f017bd6faeba6809bd6afc4775af7971ef
Dmitriy Zhevnov <dmitriy.zhevnov04@mail.ru>
2022-09-22 13:44:07 +0300

    Cosmetic fix

--- internal/model/link.go
+++ internal/model/link.go
@@ -3,10 +3,3 @@ package model
 type LinkRequest struct {
 	Url string `json:"url"`
 }
-
-// TODO
-type LinkResponse struct {
-	ID        string `json:"id"`
-	LongLink  string `json:"long_lin"`
-	ShortLink string `json:"short_link"`
-}
