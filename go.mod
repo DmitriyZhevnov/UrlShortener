e9577c916a32129667aafb0fb6948a5434ac573e
Dmitriy Zhevnov <dmitriy.zhevnov04@mail.ru>
2022-09-22 12:55:45 +0300

    Implement service logic

--- go.mod
+++ go.mod
@@ -28,6 +28,7 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
+	golang.org/x/sync v0.0.0-20220907140024-f12130a52804
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
--- internal/service/urlShortener.go
+++ internal/service/urlShortener.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/DmitriyZhevnov/UrlShortener/internal/repository"
 	"github.com/DmitriyZhevnov/UrlShortener/pkg/utils"
+	"golang.org/x/sync/errgroup"
 )
 
 type urlShortenerService struct {
@@ -22,11 +23,38 @@ func NewUrlShortenerSevice(p repository.UrlShortenerPostgres, r repository.UrlSh
 }
 
 func (s *urlShortenerService) Get(ctx context.Context, longLink string) (string, error) {
-	_, err := s.hasher.IsValidLink(longLink)
+	url, err := s.hasher.IsValidLink(longLink)
 	if err != nil {
 		// TODO
 		return "", err
 	}
 
-	return s.postgresRepository.Get(ctx, longLink)
+	shortLink, err := s.redisRepository.Get(ctx, longLink)
+	if err == nil {
+		return shortLink, nil
+	}
+
+	shortLink, err = s.postgresRepository.Get(ctx, longLink)
+	if err == nil {
+		return shortLink, nil
+	}
+
+	shortLink = s.hasher.HashURI(url)
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		return s.postgresRepository.Post(ctx, longLink, shortLink)
+	})
+
+	g.Go(func() error {
+		return s.redisRepository.Post(ctx, longLink, shortLink)
+	})
+
+	if err = g.Wait(); err != nil {
+		// TODO
+		return "", err
+	}
+
+	return shortLink, nil
 }
